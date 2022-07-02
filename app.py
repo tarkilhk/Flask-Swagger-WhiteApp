6f1cd567015c586e9dde3d7d8bdae3c2e9bc003e
tarkil <tarkil@gmail.com>
2022-07-02 18:05:30 +0800

    Switch port to using a variable

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ from logging.handlers import RotatingFileHandler
 from flask import Flask, request
 from flask_restx import Api, Resource
 
-
 # Logging mechanism
 myLogger = logging.getLogger('main')
 
-log_filename = os.path.join("log","app.log")
+log_filename = os.path.join("log", "app.log")
 os.makedirs(os.path.dirname(log_filename), exist_ok=True)
 myHandler = RotatingFileHandler(log_filename, mode='a', maxBytes=20000000, backupCount=5)
 
@@ -17,6 +16,8 @@ myHandler.setLevel(logging.INFO)
 myHandler.setFormatter(logging.Formatter('(%(threadName)s) %(asctime)s %(message)s'))
 myLogger.addHandler(myHandler)
 myLogger.setLevel(logging.INFO)
+
+PORT = 5666
 
 flask_app = Flask(__name__)
 app = Api(app=flask_app)
@@ -30,12 +31,14 @@ class Hello(Resource):
     def get(self):
         return 'Hello, World!'
 
+
 @nameSpace.route('/postSomething')
-@nameSpace.param(name="something",description="Something")
+@nameSpace.param(name="something", description="Something")
 class Something(Resource):
     def post(self):
         something = request.args['something']
         return f"Here's something : {something}", 200
 
+
 if __name__ == '__main__':
-    flask_app.run(host='0.0.0.0', port=5666)
+    flask_app.run(host='0.0.0.0', port=PORT)
